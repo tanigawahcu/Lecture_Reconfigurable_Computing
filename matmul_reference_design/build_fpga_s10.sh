c185fb152be5e826108d2a3ec290a7b576e0bd84
Kazuya Tanigawa <tanigawa@hiroshima-cu.ac.jp>
2023-11-28 11:22:01 +0900

    Reference Design のPEアレイを32x32と指定

--- matmul_reference_design/build_fpga_s10.sh
+++ matmul_reference_design/build_fpga_s10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/intel/inteloneapi/setvars.sh
-cmake .. -DSET_TILE_A=16 -DSET_TILE_B=16 -DFPGA_DEVICE=intel_s10sx_pac:pac_s10 -DIS_BSP=1
+cmake .. -DSET_TILE_A=32 -DSET_TILE_B=32 -DFPGA_DEVICE=intel_s10sx_pac:pac_s10 -DIS_BSP=1
 make fpga_emu
 #make fpga_sim
 make report
