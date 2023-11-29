d6f50f7f57685f0ddca3e5dca9a2f2f956d49a9f
Kazuya Tanigawa <tanigawa@hiroshima-cu.ac.jp>
2023-11-29 12:27:15 +0900

    DevCloud用のBuild/Runスクリプトの間違いを修正（sub→qsub）

--- matmul_reference_design/build_run.sh
+++ matmul_reference_design/build_run.sh
@@ -1,5 +1,5 @@
 # Intel DevCloud for OneAPI 用のbuild&run shell スクリプト
 rm -rf build
 mkdir build
-sub -l nodes=1:fpga_runtime:stratix10:ppn=2 -d build build_fpga_s10.sh
-sub -l nodes=1:fpga_runtime:stratix10:ppn=2 -d build run_fpga_s10.sh
+qsub -l nodes=1:fpga_runtime:stratix10:ppn=2 -d build build_fpga_s10.sh
+qsub -l nodes=1:fpga_runtime:stratix10:ppn=2 -d build run_fpga_s10.sh
